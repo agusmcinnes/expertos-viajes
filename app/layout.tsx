4ccf376d737dc135b47c0510c07f07881bcea7d3
Agustin Mc innes Alemany <agusmc@MacBook-Air-de-Agustin.local>
2025-12-26 12:47:07 -0300

    fix: meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,7 +93,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1475188480379267');
+              fbq('init', '1386050999837153');
               fbq('track', 'PageView');
             `,
           }}
@@ -103,7 +103,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1475188480379267&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1386050999837153&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
